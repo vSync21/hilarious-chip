995ee62edd91dfa6f5310525a2a34c369fc732f5
vSync21 <156121740+vSync21@users.noreply.github.com>
2024-02-06 17:15:06 -0500

    Made remediations
    Made remediations with TA in order to get the code correctly into my-card.js, and to have all 5 cards with different titles, descriptions, pictures and different background color.

--- src/my-card.js
+++ src/my-card.js
@@ -14,9 +14,11 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "the POD CARD";
-    this.src = "https://i.postimg.cc/cCkpvY73/7bd23ffa9c82eb5d82cbb6335ba20d69.jpg";
-    this.pargraph = "The image above is a picture of a man sleeping in a napping pod from the future. Hopefully someday we all can have our own napping pod.";
-    this.button = "Details";
+    this.image = "Image here";
+    this.paragraph = "Paragraph here";
+    this.link = "Link here";
+    this.color = "color of card";
+    
   }
 
 
@@ -26,6 +28,95 @@ export class MyCard extends LitElement {
       :host {
         display: inline-flex;
       }
+
+      .heading {
+        color: red;
+        font-size: 32px;
+
+      }
+      
+      img{
+      height: 280px;
+      width: 300px;
+    }
+
+    p {
+
+      color: red;
+      font-weight: bold;
+      padding: 0px 0px 8px 0;
+
+    }
+
+
+    @media screen and (min-width: 500px) and (max-width: 800px){
+    button, img {
+      display: block;
+    }
+  }
+  
+  @media screen and (max-width: 500px) {
+    #wholecard {
+      max-width: 100%;
+    }
+  }
+  :root, html, body {
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    font-size: 30px; 
+    --basic-color: #ff6500;
+  }
+  
+  
+  
+  
+  
+  
+  
+ 
+  .card {
+    align-items: center;
+    font-size: 16px;
+    display: inline-flex;
+    border: 16px solid black;
+    padding: 8px;
+    margin: 8px;
+    background-color: lightblue;
+    flex-direction: column;
+    width: 90%;
+  }
+  
+  
+  
+  button {
+    align-items: center;
+    width:300px;
+    max-width: 100%;
+    padding-top: 8px;
+    margin-top: -16px;
+    margin-bottom: 8px;
+    color: white;
+    transition: all 0.4% linear;
+    font-size: 32px;
+    background-color: red;
+    border: lightblue;
+    margin: center;
+  }
+
+  button:hover{
+    background-color: orange;
+  }
+  
+  
+
+  
+  
+  
+  
+  a {
+    text-decoration: none;
+  }
       
 
       
@@ -34,17 +125,33 @@ export class MyCard extends LitElement {
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div id = "wholecard">
+      <div class = "card" style="background-color: ${this.color};">
+        <h3 class = "heading">${this.title}</h3>
+        <img src="${this.image}" class="img-pod">
+        <p class = "paragraph">${this.paragraph}</p>
+        <a href = "${this.link}"><button class = "button">Details</button></a>
+  </div>
+
+      
+   
+  </div>
+  </div>
+    
+    `;
+
+    
     
   }
 
   static get properties() {
     return {
       title: { type: String },
-      src: {type: Image},
-      pargraph: {type: String},
-      button: {type: String},
-
+      image: {type: String},
+      paragraph: {type: String},
+      link: {type: String},
+      color: {type: String},
     };
   }
 }
